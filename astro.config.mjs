a5018b489be1b04ef3d80eeb5f626a88b0f1a045
Denilson Uariua <denuariua@gmail.com>
2024-04-15 15:39:46 +0200

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,25 @@
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig, squooshImageService } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon({
-    include: {
-      mdi: ["*"]
-    }
-  })],
+  integrations: [
+    tailwind(),
+    mdx(),
+    icon({
+      include: {
+        mdi: ["*"],
+      },
+    }),
+  ],
   image: {
-    service: squooshImageService()
+    service: squooshImageService(),
   },
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
